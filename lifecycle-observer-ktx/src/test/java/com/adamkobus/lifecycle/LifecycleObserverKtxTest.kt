118394df665cabcbb1b7d88759b23f1688beb2d5
Adam Kobus <adam.kobus@appson.org>
2022-02-05 22:48:58 +0100

    Tests config fix

--- lifecycle-observer-ktx/src/test/java/com/adamkobus/lifecycle/LifecycleObserverKtxTest.kt
+++ lifecycle-observer-ktx/src/test/java/com/adamkobus/lifecycle/LifecycleObserverKtxTest.kt
@@ -6,48 +6,53 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.Timeout
 import java.util.concurrent.TimeUnit
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class LifecycleObserverKtxTest {
 
     @JvmField
     @Rule
     val timeoutRule = Timeout(10, TimeUnit.SECONDS)
 
+    private val deferred = CompletableDeferred<Unit>()
     private val lifecycleOwner: LifecycleOwner = mockk()
     private val suspendTask: suspend () -> Unit = mockk()
     private val task: () -> Unit = mockk()
 
     @Before
     fun setUp() {
-        coEvery { suspendTask.invoke() } coAnswers {}
+        coEvery { suspendTask.invoke() } coAnswers { deferred.complete(Unit) }
         every { task.invoke() } answers {}
     }
 
     @Test
-    fun `GIVEN start task registered with runOnCreateDestroy WHEN ON_CREATE THEN task is executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnCreateDestroy WHEN ON_CREATE THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnCreateDestroy { onCreate = suspendTask }
 
         // when
         testSubject.onStateChanged(lifecycleOwner, Lifecycle.Event.ON_CREATE)
 
         // then
+        deferred.await()
         coVerify { suspendTask.invoke() }
     }
 
     @Test
-    fun `GIVEN start task registered with runOnCreateDestroy WHEN not ON_CREATE THEN task not executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnCreateDestroy WHEN not ON_CREATE THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnCreateDestroy { onCreate = suspendTask }
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_CREATE }.forEach { event ->
             // when
@@ -59,9 +64,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnCreateDestroy WHEN ON_DESTROY THEN task is executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnCreateDestroy WHEN ON_DESTROY THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnCreateDestroy { onDestroy = task }
 
         // when
@@ -72,9 +77,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnCreateDestroy WHEN not ON_DESTROY THEN task not executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnCreateDestroy WHEN not ON_DESTROY THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnCreateDestroy { onDestroy = task }
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_DESTROY }.forEach { event ->
             // when
@@ -86,22 +91,23 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN start task registered with runOnStartStop WHEN ON_START THEN task is executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnStartStop WHEN ON_START THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnStartStop { onStart = suspendTask }
 
         // when
         testSubject.onStateChanged(lifecycleOwner, Lifecycle.Event.ON_START)
 
         // then
+        deferred.await()
         coVerify { suspendTask.invoke() }
     }
 
     @Test
-    fun `GIVEN start task registered with runOnStartStop WHEN not ON_START THEN task not executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnStartStop WHEN not ON_START THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnStartStop { onStart = suspendTask }
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_START }.forEach { event ->
             // when
@@ -113,9 +119,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnStartStop WHEN ON_STOP THEN task is executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnStartStop WHEN ON_STOP THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnStartStop { onStop = task }
 
         // when
@@ -126,9 +132,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnStartStop WHEN not ON_STOP THEN task not executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnStartStop WHEN not ON_STOP THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnStartStop { onStop = task }
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_STOP }.forEach { event ->
             // when
@@ -140,22 +146,23 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN start task registered with runOnResumePause WHEN ON_RESUME THEN task is executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnResumePause WHEN ON_RESUME THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnResumePause { onResume = suspendTask }
 
         // when
         testSubject.onStateChanged(lifecycleOwner, Lifecycle.Event.ON_RESUME)
 
         // then
+        deferred.await()
         coVerify { suspendTask.invoke() }
     }
 
     @Test
-    fun `GIVEN start task registered with runOnResumePause WHEN not ON_RESUME THEN task not executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnResumePause WHEN not ON_RESUME THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnResumePause { onResume = suspendTask }
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_RESUME }.forEach { event ->
             // when
@@ -167,9 +174,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnStartStop WHEN ON_PAUSE THEN task is executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnStartStop WHEN ON_PAUSE THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnResumePause { onPause = task }
 
         // when
@@ -180,9 +187,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnStartStop WHEN not ON_PAUSE THEN task not executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnStartStop WHEN not ON_PAUSE THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnResumePause { onPause = task }
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_PAUSE }.forEach { event ->
             // when
@@ -196,22 +203,23 @@ class LifecycleObserverKtxTest {
     // single event registration
 
     @Test
-    fun `GIVEN start task registered with runOnCreate WHEN ON_CREATE THEN task is executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnCreate WHEN ON_CREATE THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnCreate(suspendTask)
 
         // when
         testSubject.onStateChanged(lifecycleOwner, Lifecycle.Event.ON_CREATE)
 
         // then
+        deferred.await()
         coVerify { suspendTask.invoke() }
     }
 
     @Test
-    fun `GIVEN start task registered with runOnCreate WHEN not ON_CREATE THEN task not executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnCreate WHEN not ON_CREATE THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnCreate(suspendTask)
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_CREATE }.forEach { event ->
             // when
@@ -223,9 +231,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnDestroy WHEN ON_DESTOY THEN task is executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnDestroy WHEN ON_DESTOY THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnDestroy(task)
 
         // when
@@ -236,9 +244,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnDestroy WHEN not ON_DESTOY THEN task not executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnDestroy WHEN not ON_DESTOY THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnDestroy(task)
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_DESTROY }.forEach { event ->
             // when
@@ -250,22 +258,23 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN start task registered with runOnStart WHEN ON_START THEN task is executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnStart WHEN ON_START THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnStart(suspendTask)
 
         // when
         testSubject.onStateChanged(lifecycleOwner, Lifecycle.Event.ON_START)
 
         // then
+        deferred.await()
         coVerify { suspendTask.invoke() }
     }
 
     @Test
-    fun `GIVEN start task registered with runOnStart WHEN not ON_START THEN task not executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnStart WHEN not ON_START THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnStart(suspendTask)
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_START }.forEach { event ->
             // when
@@ -277,9 +286,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnStop WHEN ON_STOP THEN task is executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnStop WHEN ON_STOP THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnStop(task)
 
         // when
@@ -290,9 +299,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnDestroy WHEN not ON_STOP THEN task not executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnDestroy WHEN not ON_STOP THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnStop(task)
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_STOP }.forEach { event ->
             // when
@@ -304,22 +313,23 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN start task registered with runOnResume WHEN ON_RESUME THEN task is executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnResume WHEN ON_RESUME THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnResume(suspendTask)
 
         // when
         testSubject.onStateChanged(lifecycleOwner, Lifecycle.Event.ON_RESUME)
 
         // then
+        deferred.await()
         coVerify { suspendTask.invoke() }
     }
 
     @Test
-    fun `GIVEN start task registered with runOnResume WHEN not ON_RESUME THEN task not executed`() = runBlocking {
+    fun `GIVEN start task registered with runOnResume WHEN not ON_RESUME THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnResume(suspendTask)
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_RESUME }.forEach { event ->
             // when
@@ -331,9 +341,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnPause WHEN ON_PAUSE THEN task is executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnPause WHEN ON_PAUSE THEN task is executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnPause(task)
 
         // when
@@ -344,9 +354,9 @@ class LifecycleObserverKtxTest {
     }
 
     @Test
-    fun `GIVEN stop task registered with runOnPause WHEN not ON_PAUSE THEN task not executed`() = runBlocking {
+    fun `GIVEN stop task registered with runOnPause WHEN not ON_PAUSE THEN task not executed`() = runTest {
         // given
-        val testSubject = testSubject(this)
+        val testSubject = testSubject()
         testSubject.runOnPause(task)
         Lifecycle.Event.values().filter { it != Lifecycle.Event.ON_PAUSE }.forEach { event ->
             // when
@@ -357,5 +367,5 @@ class LifecycleObserverKtxTest {
         }
     }
 
-    private fun testSubject(scope: CoroutineScope) = LifecycleObserverKtx(scope, Dispatchers.Default)
+    private fun TestScope.testSubject() = LifecycleObserverKtx(this, StandardTestDispatcher(testScheduler))
 }
